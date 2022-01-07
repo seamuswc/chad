f71096b27c21e8d3e27d4e98da242be091c6e04d
Seamus Connolly <seamusconnolly@Seamuss-MacBook-Pro.local>
2022-01-07 01:01:27 -0300

    MINT SOLUTION

--- README.md
+++ README.md
@@ -10,8 +10,9 @@
   8) **Type:** ruby chad.rb
 
 ## ISSUES
-  1) Opensea limites 50 assets per API call, so not accurate with larger wallets 
-  2) Does NOT detect MINTED assets, this is tricky, fixable at some point
+  1) Opensea limites 50 assets per API call, so not accurate with larger wallets,
+  beacuse of this limitation it makes two seperate api calls and combines them. One sorts by Sale_Price DESC and the other by Sale_Count ASC. These combine to give semi accurate picture of the accounts holdings.
+  2) MINTED assets, assets that are minted but NOT UNIQUE are currently being listed as PURCHASES.
   3) 1 Wallet address per time. We can make it loop through a txt.file of address though.
   4) **ctrl c** will exit the program at any time.
 
--- chad.rb
+++ chad.rb
@@ -5,6 +5,9 @@ require 'json'
 
 def api()
 
+    
+    #url = URI("https://api.opensea.io/api/v1/assets?owner=0xBbcFa939071D2A0BF64D8E15a715C0BE73735503&limit=50&order_by=sale_price&order_direction=desc")
+
     url = URI("https://api.opensea.io/api/v1/assets?owner=#{$wallet}&limit=50&order_by=sale_price&order_direction=desc")
 
     http = Net::HTTP.new(url.host, url.port)
@@ -45,21 +48,61 @@ def parse()
     
     data = api()
     data.each do |x|   
+        next if x["last_sale"].nil?  
         slug =  x["collection"]["slug"] 
         floor = get_floor(slug)
         last_price= x["last_sale"]["total_price"].to_f / 1000000000000000000
-        puts "#{x["asset_contract"]["name"]} was pruchsed for #{last_price} and has a current floor price of #{floor} ETH"  
+        puts "#{x["asset_contract"]["name"]} was purchased for #{last_price} and has a current floor price of #{floor} ETH"  
         $total_floor += floor
         $total_paid += last_price 
     end
 
 end
 
+def api_mints()
+
+    url = URI("https://api.opensea.io/api/v1/assets?owner=#{$wallet}&limit=50&order_by=sale_count&order_direction=asc")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+
+    res = http.request(request)
+
+    if res.code == "200"
+        parsed = JSON.parse(res.body)
+        return parsed["assets"]
+    else
+        puts "couldnt find nft"
+        puts res.code
+    end
+
+end
+
+def mints()
+    
+    data = api_mints()
+    data.each do |x|   
+        slug =  x["collection"]["slug"] 
+        floor = get_floor(slug)
+
+        if x["last_sale"].nil?
+            puts "#{x["asset_contract"]["name"]} was Minted and has a current floor price of #{floor} ETH"  
+        end
+        $total_floor += floor
+
+    end
+
+end
+
 $total_floor = 0
 $total_paid = 0
+
 puts "Enter address\n"
 $wallet = gets.chomp
 
 parse()
+mints()
 
-puts "This wallet spend a total of #{$total_paid} and is worth a total of #{$total_floor}"
+puts "This wallet spend a total of #{$total_paid} and is worth a total of #{$total_floor} on NFT's currently in the wallet"
